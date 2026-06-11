3167df88020a8641c657bdece963ea3d68313a04
Stefan Wagner <112750321+Stdehnbostel@users.noreply.github.com>
2026-06-11 09:37:34 +0200

    Initial Commit

---
+++ KaffeeTracker/KaffeeTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  KaffeeTrackerApp.swift
+//  KaffeeTracker
+//
+//  Created by Stefan on 11.06.26.
+//
+
+import SwiftUI
+
+@main
+struct KaffeeTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KaffeeTrackerTests/KaffeeTrackerTests.swift
@@ -0,0 +1,16 @@
+//
+//  KaffeeTrackerTests.swift
+//  KaffeeTrackerTests
+//
+//  Created by Stefan on 11.06.26.
+//
+
+import Testing
+
+struct KaffeeTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
