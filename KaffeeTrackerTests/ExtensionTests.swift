d77d714870f3b5e7eb3649bb73b0ba73df2d19cd
Stefan Wagner <112750321+Stdehnbostel@users.noreply.github.com>
2026-06-25 11:19:00 +0200

    test startOfWeek returns the correct date.

---
+++ KaffeeTrackerTests/ExtensionTests.swift
@@ -0,0 +1,23 @@
+//
+//  ExtensionTests.swift
+//  KaffeeTrackerTests
+//
+//  Created by Stefan on 25.06.26.
+//
+
+import Testing
+import Foundation
+@testable import KaffeeTracker
+
+struct DateExtensionsTests {
+    @Test func startOfWeekReturnsCorrectDate() {
+        let formatter = ISO8601DateFormatter()
+        // de summer time: 25.06.2026 12:00
+        let date = formatter.date(from: "2026-06-25T10:00:00Z")!
+        print(date.formatted())
+        let expectedStartOfWeek = formatter.date(from: "2026-06-21T22:00:00Z")!
+        
+        let startOfWeek = date.startOfWeek
+        #expect(startOfWeek == expectedStartOfWeek, "expected: 22.06.2026, 00:00, got: \(startOfWeek?.formatted())")
+    }
+}
