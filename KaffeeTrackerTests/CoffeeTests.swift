6816da568320fdadebf4349ac7e52a86ddada15a
Stefan Wagner <112750321+Stdehnbostel@users.noreply.github.com>
2026-07-01 14:49:12 +0200

    test coffee name is equal to type.name

--- KaffeeTrackerTests/CoffeeTests.swift
+++ KaffeeTrackerTests/CoffeeTests.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+import Testing
+import Foundation
+@testable import KaffeeTracker
+
+struct CoffeeTests {
+    @Test func nameIsIdenticalToTypeName() {
+        let type = CoffeeType(name: "Esspresso", defaultVolume: 60, defaultPrice: 2.4, defaultCaffeine: 60)
+        let coffee = Coffee(price: 2.2, volume: 70, type: type, date: .now)
+        #expect(type.name == coffee.type.name)
+        #expect(type.name == coffee.name)
+    }
+}
