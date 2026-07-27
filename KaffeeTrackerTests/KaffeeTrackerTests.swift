43581d4bde16742cb222d8b0bd5bca984fb1f0af
Stefan Wagner <112750321+Stdehnbostel@users.noreply.github.com>
2026-07-27 10:50:59 +0200

    add test

--- KaffeeTrackerTests/KaffeeTrackerTests.swift
+++ KaffeeTrackerTests/KaffeeTrackerTests.swift
@@ -5,4 +5,16 @@
 //  Created by Stefan on 27.07.26.
 //
 
+import Testing
 import Foundation
+@testable import KaffeeTracker
+
+@MainActor
+class HomeViewModelTests {
+    
+    @Test func chartDataHasSevenDays() {
+        let sut = HomeView.ViewModel()
+        let coffees = [Coffee]()
+        #expect(sut.chartData(for: coffees).count == 7, "chartData should contain 7 elements")
+    }
+}
