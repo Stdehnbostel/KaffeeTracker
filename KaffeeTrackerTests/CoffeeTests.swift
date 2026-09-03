217c4a829a4811379c9785bd2bcd7a6af08f0b74
Stefan Wagner <112750321+Stdehnbostel@users.noreply.github.com>
2026-09-03 14:41:20 +0200

    test the correctness of the convenience Initializer.

--- KaffeeTrackerTests/CoffeeTests.swift
+++ KaffeeTrackerTests/CoffeeTests.swift
@@ -12,4 +12,15 @@ import Foundation
 @testable import KaffeeTracker
 
 struct CoffeeTests {
+    let type = CoffeeType(name: "Flat White", defaultVolume: 120, defaultPrice: 4.80, defaultCaffeine: 60)
+    @Test func convenienceInitCorrecltyInitializesCoffeeFromAType() {
+        let date = Date.now
+        let coffee = Coffee(type: type, date: .now)
+        
+        #expect(coffee.name == type.name, "The coffee's name should match the type's name")
+        #expect(coffee.volume == type.defaultVolume, "The coffee's volume should match the type's default volume")
+        #expect(coffee.price == type.defaultPrice, "The coffee's price should match the type's default price")
+        #expect(coffee.type === type, "The coffee's type should be the one provided")
+        #expect(coffee.date == date, "The coffee's date should match the provided date")
+    }
 }
